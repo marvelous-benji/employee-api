da328f7b55c0eeee1da98b73e1c70c5029614447
marvelous-benji <61952411+marvelous-benji@users.noreply.github.com>
2021-05-03 08:37:36 +0100

    Added comments

--- run.py
+++ run.py
@@ -2,12 +2,12 @@ from employee_api import app, db
 from flask_migrate import Migrate
 from employee_api.models import User
 
-
-
+#this is the file that starts the application
+#needed for database migration
 migrate = Migrate(app,db)
 
 
-
+#needed to directly interact with the database objects in python object format
 @app.shell_context_processor
 def make_shell_context():
 	return dict(db=db, User=User)
